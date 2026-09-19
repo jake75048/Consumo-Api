cd04d93c506809f421bb57f17a2140a75d29bd88
Esdras <jake75048@gmail.com>
2026-09-19 12:22:01 -0300

    ajuste na busca e no banner

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-let =[];
-
-async function carregar(){
-    const resposta = await fecht('https://dummyjson.com/products');  
+let produtos = [];
+async function carregar() {
+    const resposta = await fetch('https://dummyjson.com/products');  
     const dados = await resposta.json();
-    produtos =dados.produtos;
+    produtos = dados.products;
     exibir(produtos);
+}
+function exibir(lista) {
+    const container = document.getElementById('produtos');
+    container.innerHTML = '';
+    
+    lista.forEach(produto => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+                      card.innerHTML = `
+            <img src="${produto.thumbnail}" alt="${produto.title}">
+            <h3>${produto.title}</h3>
+            <p>Preço: $${produto.price}</p>
+        `;
+        container.appendChild(card);        
+    });}
+function pesquisar() {
+        const texto = document.getElementById('campoBusca').value.toLowerCase();
+         const resultado = produtos.filter(produto => {
+        return produto.title.toLowerCase().includes(texto);
+    });
+        exibir(resultado);
+}
+document.getElementById('btnBusca').addEventListener('click', pesquisar);
+document.getElementById('campoBusca').addEventListener('keyup', pesquisar);
+carregar();
+function pesquisar() {
+    const texto = document.getElementById('campoBusca').value.toLowerCase();
+    
+    document.querySelector('.banner').style.display = texto ? 'none' : 'block';
 
-
+    const resultado = produtos.filter(p => p.title.toLowerCase().includes(texto));
+    exibir(resultado);
 }
